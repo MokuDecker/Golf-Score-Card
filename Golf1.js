282e72b75bec9fc14271592ec4bc801ec03cc9b2
Pasquale Caiazzo <plcaiazzo@gmail.com>
2020-12-09 20:18:19 -0600

    got js running again, prevent form behavior

--- Golf1.js
+++ Golf1.js
@@ -14,30 +14,38 @@ var strokes = document.getElementById('strokes');
 
 // Here is my function
 function golfScore(par, strokes) {
-    if (strokes == 1) {
-      return names[0]
-    } else if (strokes <= par - 2) {
-      return names[1]
-    } else if (strokes == par - 1) {
-      return names[2]
-    } else if (strokes == par) {
-      return names[3]
-    } else if (strokes == par + 1) {
-      return names[4]
-    } else if (strokes == par + 2) {
-      return names[5]
-    } else if (strokes >= par + 3) {
-      return names[6]
-    }
+  if (strokes == 1) {
+    return names[0]
+  } else if (strokes <= par - 2) {
+    return names[1]
+  } else if (strokes == par - 1) {
+    return names[2]
+  } else if (strokes == par) {
+    return names[3]
+  } else if (strokes == par + 1) {
+    return names[4]
+  } else if (strokes == par + 2) {
+    return names[5]
+  } else if (strokes >= par + 3) {
+    return names[6]
   }
-  
-  // Change these values to test
+}
 
-  function produceMessage(){
-    var msg= golfScore();
-    return msg;
-  }
+// Selects form on page (might want to be more specific), adds event listener that listens for submit event,
+// prevents default behavior.
+var form = document.querySelector('form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log("I am no longer refreshing the damn page!");
+});
   
-  window.alert(golfScore());
-  document.write(golfScore());
-  console.log(golfScore(par,strokes));
+// Change these values to test
+
+function produceMessage(){
+  var msg= golfScore();
+  return msg;
+}
+
+window.alert(golfScore());
+document.write(golfScore());
+console.log(golfScore(par,strokes));
